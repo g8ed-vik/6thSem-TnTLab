7672615db41d33816cdb63bfa5d6f13ed1076c32
Souvik Pal <84220139+g8ed-vik@users.noreply.github.com>
2023-02-28 00:24:32 +0530

    Add files via upload

---
+++ 2nd February/q8.py
@@ -0,0 +1,9 @@
+#WAP in Python to calculate x y by writing a function (say POWER) for it.
+
+def power(x,y):
+    x = int(input("Enter the base number: "))
+    y = int(input("Enter the power number: "))
+    return x**y
+
+a = power(0,0)
+print(a)
